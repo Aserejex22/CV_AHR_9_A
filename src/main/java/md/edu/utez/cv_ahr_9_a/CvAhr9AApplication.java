42c029fc776e1ddf66820ded9db8629fe6138deb
Aserejex22 <20223tn097@utez.edu.mx>
2025-05-29 21:55:48 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/md/edu/utez/cv_ahr_9_a/CvAhr9AApplication.java
@@ -0,0 +1,13 @@
+package md.edu.utez.cv_ahr_9_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvAhr9AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvAhr9AApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md/edu/utez/cv_ahr_9_a/CvAhr9AApplicationTests.java
@@ -0,0 +1,13 @@
+package md.edu.utez.cv_ahr_9_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAhr9AApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
